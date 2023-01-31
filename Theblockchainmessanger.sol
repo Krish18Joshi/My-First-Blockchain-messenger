69b99c2b8e20637e21399c397d79f8c2a6e1204e
Krish18Joshi <99706585+Krish18Joshi@users.noreply.github.com>
2023-01-31 12:44:30 +0530

    messager by using solidity

---
+++ Theblockchainmessanger.sol
@@ -0,0 +1,28 @@
+
+//SPDX-License-Identifier: MIT
+
+pragma solidity 0.8.14;
+// NAME => KRISH JOSHI
+contract TheBlockchainMessenger {
+
+    uint public changeCounter;
+
+    address public owner;
+
+    string public theMessage;
+
+    constructor() {
+        owner = msg.sender;
+    }
+
+    function updateTheMessage(string memory _newMessage) public {
+        if(msg.sender == owner) {
+            theMessage = _newMessage;
+            changeCounter++;
+        }
+    }
+}
+
+
+
+
